18c705d9e35a7551d27d22fffbe25488d1f470e2
Sujian Xue <sxue0783@uni.sydney.edu.au>
2024-10-28 13:31:28 +1100

    add region

---
+++ 5047/src/global.h
@@ -0,0 +1,17 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+// I2S pins for ESP32 (for the MAX98357A amplifier)
+#define I2S_BCLK 4   // Bit Clock pin
+#define I2S_LRCLK 12 // Left-Right Clock pin
+#define I2S_DIN 22   // Data Input pin
+
+#define SWITCH_PIN 35
+
+#define S0 13
+#define S1 14
+#define S2 0
+#define S3 26
+#define OUT_PIN 25
+
+#endif
--- 5047/src/google_tts.h
+++ 5047/src/google_tts.h
@@ -9,7 +9,9 @@ extern bool requesting;
 extern WiFiClient *stream;
 extern size_t bytesRead;
 extern uint8_t buffer[512];
+extern HTTPClient http_tts_result;
 
+#pragma region Process_by_ESP32
 // void base64UrlEncode(const unsigned char *input, size_t length, String &output);
 // void signJWT(const String &message, const char *private_key, String &signature);
 // bool SetupNTPTime();
@@ -17,11 +19,13 @@ extern uint8_t buffer[512];
 // String getAccessToken(const String &jwt);
 // String requestGoogleTTS(const String &text, const String &accessToken);
 // String GetJWTToken();
-bool RequestBackendTTS(String &text);
-void EndHTTPTTSResult();
 // void SetAudioConfig(int I2S_BCLK, int I2S_LRCLK, int I2S_DIN, int volume = 21);
 // void AudioLoop();
-void Pause();
-void Loop();
+#pragma endregion
+
+bool RequestBackendTTS(String &text);
+void EndHTTPTTSResult();
+void TTS_Pause();
+void TTS_Loop();
 
 #endif  // GOOGLE_TTS_H
